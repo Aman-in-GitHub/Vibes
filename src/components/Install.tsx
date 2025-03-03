bf9e851a41ab41d080844e9ee287733fae079079
Aman Chand <amanchandinc@gmail.com>
2025-03-04 01:05:43 +0545

    Fix: PWA install button 4.0

--- src/components/Install.tsx
+++ src/components/Install.tsx
@@ -1,10 +1,14 @@
-import { useEffect, useRef } from 'react';
+// @ts-nocheck
+
+import logo from '@/logo.svg';
+import { create } from 'zustand';
+import { useRef } from 'react';
 import {
   PiAppStoreLogo as AppStore,
   PiGooglePlayLogo as PlayStore
 } from 'react-icons/pi';
 import { isIOS } from 'react-device-detect';
-import { create } from 'zustand';
+import { PWAInstallElement } from '@khmyznikov/pwa-install';
 
 import { persist } from 'zustand/middleware';
 
@@ -26,51 +30,32 @@ export const useIsPWAInstalledStore = create(
 );
 
 function InstallButton({ onClick }: { onClick: () => void }) {
-  const deferredPrompt = useRef<any>(null);
-  const isPWAInstalled = useIsPWAInstalledStore(
-    (state) => state.isPWAInstalled
-  );
-  const setIsPWAInstalled = useIsPWAInstalledStore(
-    (state) => state.setIsPWAInstalled
-  );
-
-  useEffect(() => {
-    const handlePrompt = (e: any) => {
-      e.preventDefault();
-      deferredPrompt.current = e;
-    };
-
-    window.addEventListener('beforeinstallprompt', handlePrompt);
-    return () =>
-      window.removeEventListener('beforeinstallprompt', handlePrompt);
-  });
-
-  const handleInstallClick = () => {
-    onClick();
-    if (!deferredPrompt.current) return;
-
-    deferredPrompt.current.prompt();
-    deferredPrompt.current.userChoice.then((choiceResult: any) => {
-      if (choiceResult.outcome === 'accepted') {
-        setIsPWAInstalled(true);
-      }
-    });
-  };
-
-  if (isPWAInstalled) return null;
+  const pwaInstallRef = useRef<PWAInstallElement>(null);
 
   return (
-    <button
-      className="flex w-full items-center justify-between bg-green-600 px-4 py-4 text-xl text-white duration-300 active:bg-green-500"
-      onClick={handleInstallClick}
-    >
-      Install Vibes
-      {isIOS ? (
-        <AppStore className="text-3xl text-green-200" />
-      ) : (
-        <PlayStore className="text-3xl text-green-200" />
-      )}
-    </button>
+    <>
+      <button
+        className="flex w-full items-center justify-between bg-green-600 px-4 py-4 text-xl text-white duration-300 active:bg-green-500"
+        onClick={() => {
+          onClick();
+          pwaInstallRef.current?.showDialog(true);
+        }}
+      >
+        Install Vibes
+        {isIOS ? (
+          <AppStore className="text-3xl text-green-200" />
+        ) : (
+          <PlayStore className="text-3xl text-green-200" />
+        )}
+      </button>
+
+      <PWAInstallElement
+        ref={pwaInstallRef}
+        name={'Vibes'}
+        icon={logo}
+        disable-install-description={true}
+      />
+    </>
   );
 }
 
