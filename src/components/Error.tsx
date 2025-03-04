df580bce1afc48ceadb2c97b2babc43edc7a4ab1
Aman Chand <amanchandinc@gmail.com>
2025-03-04 21:52:46 +0545

    Fix: Imrpoved 404 & error page

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,15 +1,15 @@
 function Error({ error, reset }: { error: Error; reset: () => void }) {
   return (
-    <section className="bg-background motion-opacity-in motion-duration-[2s] flex min-h-screen w-full flex-col items-center justify-center gap-6 overflow-hidden lg:gap-12">
+    <section className="bg-background motion-opacity-in motion-blur-in motion-duration-1000 flex min-h-screen w-full flex-col items-center justify-center gap-4 overflow-hidden px-4 lg:gap-12">
       <img
-        src="/logo.svg"
-        alt="Vibes"
-        className="motion-preset-spin motion-duration-1000 size-40 select-none lg:size-52"
+        src="/maskable-icon-512x512.png"
+        alt="Error - Vibes"
+        className="size-80 rounded-xs select-none lg:size-96"
       />
 
       <div className="text-center text-red-500">
-        <p className="text-xl font-bold">Something went wrong!</p>
-        <p className="mt-1">{error.message}</p>
+        <p className="text-2xl font-bold">Something went wrong!</p>
+        <p className="mt-1 text-neutral-400">{error.message}</p>
       </div>
 
       <button onClick={reset} className="rounded-xs bg-red-600 px-4 py-2">
--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -7,11 +7,11 @@ function NotFound() {
   }, []);
 
   return (
-    <main className="bg-background motion-opacity-in motion-duration-[2s] flex min-h-screen w-full flex-col items-center justify-center gap-6 overflow-hidden lg:gap-12">
+    <main className="bg-background motion-opacity-in motion-blur-in motion-duration-1000 flex min-h-screen w-full flex-col items-center justify-center gap-4 overflow-hidden px-4 lg:gap-12">
       <img
-        src="/logo.svg"
-        alt="Vibes"
-        className="motion-preset-spin motion-duration-1000 size-40 select-none lg:size-52"
+        src="/404.svg"
+        alt="404 - Vibes"
+        className="size-80 select-none lg:size-96"
       />
 
       <Link to="/" className="rounded-xs bg-rose-600 px-4 py-2" replace={true}>
